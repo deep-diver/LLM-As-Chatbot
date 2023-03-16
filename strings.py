57a01e0602ce4a0ace6fffc90d13ca2e5e8d55ea
Chansung Park <deep.diver.csp@gmail.com>
2023-03-17 00:10:23 +0900

    Update strings.py

--- strings.py
+++ strings.py
@@ -5,3 +5,10 @@ Thanks to tolen, this simple application runs Alpaca-LoRA which is instruction f
 
 I am thankful to the [Jarvislabs.ai](https://jarvislabs.ai/) for providing free GPU instances. 
 """
+
+BOTTOM_LINE = """
+In order to process batch generation, the common parameters in LLaMA are fixed as below:
+- temperature=0.90
+- top_p=0.75
+- num_beams=4
+"""
