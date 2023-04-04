adbc68f34622f5fef04a16b974370f4ab7d1fa2a
Chansung Park <deep.diver.csp@gmail.com>
2023-04-05 00:21:41 +0900

    remove match~case

--- utils.py
+++ utils.py
@@ -2,10 +2,9 @@ from chats import alpaca
 from chats import baize
 
 def get_chat_interface(model_type, batch_enabled):
-    match model_type:
-        case 'alpaca':
-            return alpaca.chat_batch if batch_enabled else alpaca.chat_stream
-        case 'baize':
-            return baize.chat_batch if batch_enabled else baize.chat_stream
-        case other:
-            return None
+    if model_type == "alpaca":
+        return alpaca.chat_batch if batch_enabled else alpaca.chat_stream
+    elif model_type == "baize":
+        return baize.chat_batch if batch_enabled else baize.chat_stream
+    else:
+        return None
