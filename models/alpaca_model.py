67e8854f8f6f9c96c1cf99e118ee998a045755db
zhouyu5 <yu.zhou@intel.com>
2023-04-18 13:22:23 +0000

    update app

--- models/alpaca_model.py
+++ models/alpaca_model.py
@@ -1,39 +1,70 @@
+import sys
 import torch
 from peft import PeftModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def load_model(base, finetuned, multi_gpu, force_download_ckpt):
-    tokenizer = AutoTokenizer.from_pretrained(base)
+def load_model(base_model, lora_weights, multi_gpu, force_download_ckpt, load_8bit=False):
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
     tokenizer.pad_token_id = 0
     tokenizer.padding_side = "left"
 
-    if not multi_gpu:
-        model = AutoModelForCausalLM.from_pretrained(
-            base,
-            load_in_8bit=True,
-            device_map="auto",
-        )
-        
-        model = PeftModel.from_pretrained(
-            model, 
-            finetuned, 
-            force_download=force_download_ckpt,
-            device_map={'': 0}
-        )
-        return model, tokenizer
+    if torch.cuda.is_available():
+        device = "cuda"
     else:
+        device = "cpu"
+    try:
+        if torch.backends.mps.is_available():
+            device = "mps"
+    except:
+        pass     
+
+    model_revision = "main"
+    if 'gpt-j' in base_model:
+        model_revision = "float16"
+    
+    if device == "cuda":
         model = AutoModelForCausalLM.from_pretrained(
-            base,
+            base_model,
+            revision=model_revision,
+            load_in_8bit=load_8bit,
             torch_dtype=torch.float16,
             device_map="auto",
         )
-        
         model = PeftModel.from_pretrained(
-            model, 
-            finetuned, 
-            force_download=force_download_ckpt,
-            torch_dtype=torch.float16
+            model,
+            lora_weights,
+            torch_dtype=torch.float16,
         )
-        model.half()
-        return model, tokenizer        
+    elif device == "mps":
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model,
+            revision=model_revision,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model, 
+            revision=model_revision,
+            device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            lora_weights,
+            device_map={"": device},
+        )
 
+    if not load_8bit and torch.cuda.is_available():
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    return model, tokenizer
