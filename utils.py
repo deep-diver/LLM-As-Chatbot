501ae0d88b9e37861d2f9ea492f6ce910e7f2bf0
chansung <deep.diver.csp@gmail.com>
2023-04-22 06:19:35 +0900

    add koalpaca interface

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from chats import alpaca_gpt4
 from chats import stablelm
 from chats import koalpaca
 from pingpong.gradio import GradioAlpacaChatPPManager
+from pingpong.gradio import GradioKoAlpacaChatPPManager
 from pingpong.gradio import GradioStableLMChatPPManager
 
 def get_chat_interface(model_type):
@@ -25,6 +26,6 @@ def get_chat_manager(model_type):
     elif model_type == "stablelm":
         return GradioStableLMChatPPManager()
     elif model_type == "koalpaca-polyglot":
-        return GradioAlpacaChatPPManager()
+        return GradioKoAlpacaChatPPManager()
     else:
         return None
