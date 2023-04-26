78dc3d2fd61503bd83bd2cc59cc735395a8bb61c
chansung <deep.diver.csp@gmail.com>
2023-04-27 08:40:43 +0900

    add flan and os-stablelm support

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ from chats import alpaca
 from chats import alpaca_gpt4
 from chats import stablelm
 from chats import koalpaca
+from chats import os_stablelm
 from pingpong.gradio import GradioAlpacaChatPPManager
 from pingpong.gradio import GradioKoAlpacaChatPPManager
 from pingpong.gradio import GradioStableLMChatPPManager
+from pingpong.gradio import GradioFlanAlpacaChatPPManager
+from pingpong.gradio import GradioOSStableLMChatPPManager
 
 def get_chat_interface(model_type):
     if model_type == "alpaca":
@@ -13,8 +16,12 @@ def get_chat_interface(model_type):
         return alpaca_gpt4.chat_stream
     elif model_type == "stablelm":
         return stablelm.chat_stream
+    elif model_type == "os-stablelm":
+        return os_stablelm.chat_stream
     elif model_type == "koalpaca-polyglot":
         return koalpaca.chat_stream
+    elif model_type == "flan-alpaca":
+        return flan_alpaca.chat_stream
     else:
         return None
 
@@ -25,7 +32,11 @@ def get_chat_manager(model_type):
         return GradioAlpacaChatPPManager()
     elif model_type == "stablelm":
         return GradioStableLMChatPPManager()
+    elif model_type == "os-stablelm":
+        return GradioOSStableLMChatPPManager()
     elif model_type == "koalpaca-polyglot":
         return GradioKoAlpacaChatPPManager()
+    elif model_type == "flan-alpaca":
+        return GradioFlanAlpacaChatPPManager()
     else:
         return None
