f7a2847778011da2a126aaaf22f8c35859959f30
chansung <deep.diver.csp@gmail.com>
2023-04-27 08:39:47 +0900

    update to conditioning flan and os-stablelm

--- chats/central.py
+++ chats/central.py
@@ -1,6 +1,8 @@
 from chats import stablelm
 from chats import alpaca
 from chats import koalpaca
+from chats import flan_alpaca
+from chats import os_stablelm
 
 def chat_stream(
     user_message, state,
@@ -28,6 +30,22 @@ def chat_stream(
 
     elif model_type == "koalpaca-polyglot":
         cs = koalpaca.chat_stream(
+            user_message, state,
+            ctx_num_lconv, ctx_sum_prompt,
+            res_temp, res_topp, res_topk, res_rpen, res_mnts, res_beams, res_cache, res_sample, res_eosid, res_padid,
+            sum_temp, sum_topp, sum_topk, sum_rpen, sum_mnts, sum_beams, sum_cache, sum_sample, sum_eosid, sum_padid
+        )
+
+    elif model_type == "flan-alpaca":
+        cs = flan_alpaca.chat_stream(
+            user_message, state,
+            ctx_num_lconv, ctx_sum_prompt,
+            res_temp, res_topp, res_topk, res_rpen, res_mnts, res_beams, res_cache, res_sample, res_eosid, res_padid,
+            sum_temp, sum_topp, sum_topk, sum_rpen, sum_mnts, sum_beams, sum_cache, sum_sample, sum_eosid, sum_padid
+        )        
+
+    elif model_type == "os-stablelm":
+        cs = os_stablelm.chat_stream(
             user_message, state,
             ctx_num_lconv, ctx_sum_prompt,
             res_temp, res_topp, res_topk, res_rpen, res_mnts, res_beams, res_cache, res_sample, res_eosid, res_padid,
