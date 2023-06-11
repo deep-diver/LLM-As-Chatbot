85ef135a4b168f3dce4935e55b944a349cfc41c6
chansung <deep.diver.csp@gmail.com>
2023-06-11 13:09:39 +0000

    fix: remove force download option

--- models/byom.py
+++ models/byom.py
@@ -6,12 +6,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def load_model(
     base, 
     finetuned, 
-    mode_cpu=mode_cpu,
-    mode_mps=mode_mps,
-    mode_full_gpu=mode_full_gpu,
-    mode_8bit=mode_8bit,
-    mode_4bit=mode_4bit,
-    force_download_ckpt,
+    mode_cpu,
+    mode_mps,
+    mode_full_gpu,
+    mode_8bit,
+    mode_4bit,
+    # force_download_ckpt,
     model_cls,
     tokenizer_cls
 ):
