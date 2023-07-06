8261ba7ac1826a9e883990379bcdcff8e492d236
Chansung Park <deep.diver.csp@gmail.com>
2023-07-06 10:38:10 +0900

    Update entry_point.py

--- entry_point.py
+++ entry_point.py
@@ -8,9 +8,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
     app_mode = os.getenv("LLMCHAT_APP_MODE")
+    local_files_only = os.getenv("LLMCHAT_LOCAL_FILES_ONLY")
+    
     if app_mode is None or \
         app_mode not in ["GRADIO", "DISCORD"]:
         app_mode = "GRADIO"
+
+    if local_files_only is None:
+        local_files_only = False
+    else:
+        local_files_only = bool(local_files_only)
     
     if app_mode == "GRADIO":
         parser.add_argument('--root-path', default="")
@@ -29,6 +36,6 @@ if __name__ == "__main__":
         parser.add_argument('--mode-8bit', default=False, action=argparse.BooleanOptionalAction)
         parser.add_argument('--mode-4bit', default=False, action=argparse.BooleanOptionalAction)
         parser.add_argument('--mode-full-gpu', default=True, action=argparse.BooleanOptionalAction)
-        parser.add_argument('--local-files-only', default=False, action=argparse.BooleanOptionalAction)
+        parser.add_argument('--local-files-only', default=local_files_only, action=argparse.BooleanOptionalAction)
         args = parser.parse_args()
         discord_main(args)
